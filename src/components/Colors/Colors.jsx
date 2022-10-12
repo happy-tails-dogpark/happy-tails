6d3ecc0173e54fec0ecb11a5f5d6097a0e0818f2
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-10-12 15:51:06 -0700

    setting up base template

--- src/components/Colors/Colors.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './Colors.css';
-
-export default function Colors() {
-  return (
-    <div className={styles.Colors}>Colors</div>
-  );
-}
--- src/components/Orange/Orange.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './Orange.css';
-
-export default function Orange() {
-  return (
-    <div className={styles.Orange}>Orange</div>
-  );
-}
