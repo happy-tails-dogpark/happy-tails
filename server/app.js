578d507c97d6ad6abf7d34195ebea5ef2fb82c22
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-10-13 17:25:08 -0700

    handle html 5 routes

--- server/app.js
+++ server/app.js
@@ -18,8 +18,12 @@ app.use('/api/v1/users', require('./controllers/users'));
 
 // Error handling & 404 middleware for when
 // a request doesn't match any app routes
-app.use(require('./middleware/not-found'));
+// app.use(require('./middleware/not-found'));
 app.use(require('./middleware/error'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(res.resolve (__dirname + '../dist/index.html'));
+});
 
 const pool = require('./utils/pool');
 
