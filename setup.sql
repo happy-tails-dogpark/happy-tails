4d25719fd5344282f896601f77d2a148e9bd1a83
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-10-17 12:05:47 -0600

    park controller started

--- setup.sql
+++ setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS parks CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -6,6 +7,17 @@ CREATE TABLE users (
   password_hash VARCHAR NOT NULL
 );
 
+CREATE TABLE parks (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  park_name VARCHAR,
+  park_location VARCHAR,
+  park_description VARCHAR,
+  park_image VARCHAR
+);
+
 INSERT INTO users (email, password_hash) VALUES
 ('test@gmail.com', '123456'),
-('test1@gmail.com', 'password')
+('test1@gmail.com', 'password');
+
+INSERT INTO parks (park_name, park_location, park_image, park_description) VALUES
+('my park','boulder','insert photo here','mock info for testing')
