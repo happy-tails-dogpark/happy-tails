6610e3a153defd189b8106aa4dc682bab6f7cced
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-10-21 09:23:03 -0700

    hamburger appearing and dissapearing appropriately

--- client/components/page/SlideoutMenu.jsx
+++ client/components/page/SlideoutMenu.jsx
@@ -1,11 +1,15 @@
 
 import { useState, useEffect } from 'react';
+// import { getLocalUser } from '../../services/auth';
+import { useUser } from '../../state/UserContext';
 import Navigation from './navigation.jsx';
 import styles from './SlideoutMenu.css';
 import classnames from 'classnames';
 
 export default function SlideoutMenu({ navigation }) {
   const [isOpen, setIsOpen] = useState(false);
+  // const [, setUser] = useState(localStorage.getItem('JWT.token'));
+  const user = useUser();
 
   const className = classnames(styles.SlideoutMenu, {
     [styles.Open]: isOpen,
@@ -15,6 +19,14 @@ export default function SlideoutMenu({ navigation }) {
     e.stopPropagation();
     setIsOpen((isOpen) => !isOpen);
   };
+
+  // useEffect(() => {
+  //   async function checkUser() {
+  //     const currentUser = await getLocalUser();
+  //     setUser(currentUser);
+  //   } checkUser();
+  // }, []
+  // );
 
   useEffect(() => {
     if (!isOpen) return;
@@ -34,7 +46,7 @@ export default function SlideoutMenu({ navigation }) {
     };
   }, [isOpen]);
 
-  return (
+  if (user) return (
     <button className={className} onClick={handleClick}>
       <div className={styles.MenuContainer}>
         <Navigation navigation={navigation} />
